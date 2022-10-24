db82e6a0e6472aaf2c0255e03c82d8fc525941b3
skyclouds2001 <skyclouds2001@163.com>
2022-10-24 15:57:03 +0800

    feat: update index page

--- pages/index/index.ts
+++ pages/index/index.ts
@@ -1,12 +1,6 @@
-Page<{
-  recommends: Array<string>,
-  issues: Array<string>
-}, {}>({
+Page<{}, {}>({
 
-  data: {
-    recommends: ['test', 'test-test', 'test-test-test-test'],
-    issues: ['测量不精准', '指示灯不亮', '报警音减弱或不响', '触摸屏失效', '屏幕损坏']
-  },
+  data: {},
 
   onLoad () {}
 
