0090900a36525c66717b370b958164aaa04824be
skyclouds2001 <skyclouds2001@163.com>
2022-10-24 16:41:11 +0800

    feat: update type declare

--- service/service.ts
+++ service/service.ts
@@ -1,12 +1,12 @@
 import { HOST } from '../config/config'
 
-export const request = (params: WechatMiniprogram.RequestOption): Promise<WechatMiniprogram.RequestSuccessCallbackResult> => {
+export const request = <T>(params: WechatMiniprogram.RequestOption): Promise<WechatMiniprogram.RequestSuccessCallbackResult<T>> => {
   return new Promise((resolve, reject) => {
     wx.request({
       ...params,
       url: HOST + params.url,
-      success: result => resolve(result),
-      fail: error => reject(error)
+      success: (result: WechatMiniprogram.RequestSuccessCallbackResult<T>) => resolve(result),
+      fail: (error: WechatMiniprogram.Err) => reject(error)
     })
   })
 }
