fc6cb5ea1948920e5bf9877fb7b3fec74e7a069f
skyclouds2001 <skyclouds2001@163.com>
2022-11-10 14:47:13 +0800

    feat: update show common issues

--- pages/commonIssues/commonIssues.ts
+++ pages/commonIssues/commonIssues.ts
@@ -1,7 +1,32 @@
-Page<{}, {}>({
+Page<{
+  value: string,
+  options: Array<Record<'text' | 'value', string>>,
+  currentType: string
+}, {
+  handleIssueChange: (e: VantChange) => void
+}>({
 
-  data: {},
+  data: {
+    value: 'default',
+    options: [
+      {
+        text: 'id-0',
+        value: 'id-0',
+      },
+      {
+        text: 'id-1',
+        value: 'id-1',
+      }
+    ],
+    currentType: ''
+  },
 
   onLoad() {},
+
+  handleIssueChange(e) {
+    this.setData({
+      value: e.detail as string
+    })
+  }
 
 })
