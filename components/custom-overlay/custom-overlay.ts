e598bfb2a29aa56ef9adde95db54ff681c10abbc
skyclouds2001 <skyclouds2001@163.com>
2022-11-11 00:00:24 +0800

    feat: add custom-overlay

---
+++ components/custom-overlay/custom-overlay.ts
@@ -0,0 +1,19 @@
+Component<{}, {
+  show: {
+    value: boolean,
+    type: BooleanConstructor
+  }
+}, {}>({
+  
+  properties: {
+    show: {
+      value: false,
+      type: Boolean
+    }
+  },
+
+  data: {},
+
+  methods: {}
+  
+})
