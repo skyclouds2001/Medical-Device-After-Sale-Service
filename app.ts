292ab4a20eb02c48e974940c753d804f3c96b664
skyclouds2001 <skyclouds2001@163.com>
2023-01-09 18:13:16 +0800

    feat: update app login

--- app.ts
+++ app.ts
@@ -1,7 +1,16 @@
+import { login } from '@/apis/index'
+
 App<App>({
 
   globalData: {},
 
-  onLaunch () {}
+  async onLaunch () {
+    try {
+      const { code } = await wx.login()
+      const res = await login(code)
+      console.log(res)
+      if (res.code === 0) {} else if (res.code === 1070) {} else {}
+    } catch {}
+  }
 
 })
--- lib/request.ts
+++ lib/request.ts
@@ -1,11 +1,11 @@
 import { SERVER_HOST } from '@/config/index'
 
 export const request = <T>(params: WechatMiniprogram.RequestOption<Response<T>>) => {
-  return new Promise((resolve, reject) => {
+  return new Promise<Response<T>>((resolve, reject) => {
     wx.request({
       ...params,
       url: SERVER_HOST + params.url,
-      success: (result) => resolve(result.data),
+      success: (result) => resolve(result.data as Response<T>),
       fail: (error) => reject(error)
     })
   })
