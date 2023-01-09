c7b0ad52a56953be68d07ead43dbf766b40bae2f
skyclouds2001 <skyclouds2001@163.com>
2023-01-09 19:31:21 +0800

    fix: add type define

--- lib/request.ts
+++ lib/request.ts
@@ -2,10 +2,10 @@ import { SERVER_HOST } from '@/config/index'
 
 export const request = <T>(params: WechatMiniprogram.RequestOption<Response<T>>) => {
   return new Promise<Response<T>>((resolve, reject) => {
-    wx.request({
+    wx.request<Response<T>>({
       ...params,
       url: SERVER_HOST + params.url,
-      success: (result) => resolve(result.data as Response<T>),
+      success: (result) => resolve(result.data),
       fail: (error) => reject(error)
     })
   })
