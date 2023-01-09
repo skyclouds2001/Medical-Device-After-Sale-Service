33d002b6c26248e6a12e679a51ead5cdedcfaabd
skyclouds2001 <skyclouds2001@163.com>
2023-01-09 17:45:35 +0800

    feat: update api

---
+++ apis/admin.ts
@@ -0,0 +1,26 @@
+import { request } from '@/lib'
+
+interface LoginResponse {
+  customer_id: number
+  token: string
+}
+
+export const login = (code: string) => {
+  return request<LoginResponse>({
+    url: `/wizz/aftersale/account/customer/login/${code}`,
+    method: 'GET',
+  })
+}
+
+export const auth = (id: number, name: string, phone: string, openid: string) => {
+  return request<undefined>({
+    url: '/wizz/aftersale/account/customer/authenticate',
+    method: 'GET',
+    data: {
+      company_id: id,
+      customer_name: name,
+      mobile: phone,
+      open_id: openid,
+    },
+  })
+}
