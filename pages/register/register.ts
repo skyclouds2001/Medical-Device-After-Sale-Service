e797ec7445c3ced1948ae27d5fa7d86ed7193178
skyclouds2001 <skyclouds2001@163.com>
2023-01-11 20:13:15 +0800

    feat: update register page

--- pages/register/register.ts
+++ pages/register/register.ts
@@ -1,7 +1,62 @@
-Page({
+import { login, auth } from '@/apis/admin'
+import Toast from '@vant/weapp/toast/toast'
 
-  data: {},
+Page<{
+  companies: any[],
+  show: boolean,
 
-  onLoad() {},
+  phone: string,
+  password: string,
+  company: string,
+}, {
+  userLogin: () => Promise<void>,
+  userRegister: () => Promise<void>,
+
+  handleManageSelector: (e: WechatMiniprogram.TouchEvent<{ value: string }, { flag: boolean }>) => void,
+}>({
+
+  data: {
+    companies: ['666', '777'],
+    show: false,
+
+    phone: '',
+    password: '',
+    company: '',
+  },
+
+  onLoad() {
+    this.userLogin()
+  },
+
+  handleManageSelector(e) {
+    console.log(e)
+    if (e.type === 'confirm') {
+      this.setData({
+        company: e.detail.value,
+      })
+    }
+    this.setData({
+      show: Boolean(e.mark?.flag),
+    })
+  },
+
+  async userLogin() {
+    const { code } = await wx.login()
+    const res = await login(code)
+    if (res.code === 0) {
+      wx.switchTab({
+        url: '/pages/index/index',
+      })
+    } else if (res.code === 1070) {
+      Toast('请先注册')
+    } else {
+      Toast.fail(res.data.toString())
+    }
+  },
+
+  async userRegister() {
+    const { phone, password, company } = this.data
+    console.log(phone, password, company)
+  },
 
 })
