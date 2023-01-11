178ad391f4c28558e8aff52ecd085410e65eaa13
skyclouds2001 <skyclouds2001@163.com>
2023-01-12 00:05:07 +0800

    feat: update request header

--- lib/request.ts
+++ lib/request.ts
@@ -1,10 +1,19 @@
-import { SERVER_HOST } from '@/config/index'
+import { SERVER_HOST, WHITE_LIST } from '@/config/index'
 
 export const request = <T>(params: WechatMiniprogram.RequestOption<Response<T>>) => {
+  const header = WHITE_LIST.includes(params.url) ? [] : [
+    {
+      Authorization: wx.getStorageSync('token'),
+    },
+  ]
   return new Promise<Response<T>>((resolve, reject) => {
     wx.request<Response<T>>({
       ...params,
       url: SERVER_HOST + params.url,
+      header: {
+        ...params.header,
+        ...header,
+      },
       success: (result) => resolve(result.data),
       fail: (error) => reject(error)
     })
