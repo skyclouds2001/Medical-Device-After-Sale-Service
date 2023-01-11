5fbe2a5586987e97952a924b779b3a81a789756a
skyclouds2001 <skyclouds2001@163.com>
2023-01-11 17:54:19 +0800

    feat: add register page

--- app.ts
+++ app.ts
@@ -1,16 +1,7 @@
-import { login } from '@/apis/admin'
-
 App<App>({
 
   globalData: {},
 
-  async onLaunch () {
-    try {
-      const { code } = await wx.login()
-      const res = await login(code)
-      console.log(res)
-      if (res.code === 0) {} else if (res.code === 1070) {} else {}
-    } catch {}
-  }
+  onLaunch () {},
 
 })
