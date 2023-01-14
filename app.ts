b8d0ee4dd5cb09df5ce1b4e966bacd640a6085c0
skyclouds2001 <skyclouds2001@163.com>
2023-01-15 01:09:08 +0800

    fix: incorrect get storage method

--- app.ts
+++ app.ts
@@ -11,10 +11,10 @@ App<App>({
 
   onLaunch () {
     const [token, id, userinfo] = wx.batchGetStorageSync(['token', 'id', 'userinfo']) as [string, number, UserInfo]
-    this.globalData.token = token
-    this.globalData.id = id
-    this.globalData.userinfo.nickname = userinfo.nickname ?? '点击登录'
-    this.globalData.userinfo.avatar = userinfo.avatar ?? '/icons/user.svg'
+    this.globalData.token = token ?? ''
+    this.globalData.id = id ?? ''
+    this.globalData.userinfo.nickname = userinfo?.nickname ?? '点击登录'
+    this.globalData.userinfo.avatar = userinfo?.avatar ?? '/icons/user.svg'
   },
 
 })
