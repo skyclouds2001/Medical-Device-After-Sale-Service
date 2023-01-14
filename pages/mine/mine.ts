65a6a6d76fc32dbab0c0ee9cde34a2f82a55edc2
skyclouds2001 <skyclouds2001@163.com>
2023-01-14 20:59:52 +0800

    feat: update mine page

--- pages/mine/mine.ts
+++ pages/mine/mine.ts
@@ -1,13 +1,84 @@
-Page<{}, {}>({
+import Toast from '@vant/weapp/toast/toast'
 
-  data: {},
+const app = getApp<App>()
 
-  onLoad () {},
+Page<{
+  nickname: string
+  avatar: string
+  show: boolean
+}, {
+  showLoginDialog: () => void
+  confirmLogin: (e: WechatMiniprogram.CustomEvent<{
+    avatar: string
+    nickname: string
+  }>) => void
+  editPassword: () => void
+  editBindPhone: () => void
+  exitLogin: () => void
+}>({
+
+  data: {
+    nickname: '点击登录',
+    avatar: '/icons/user.svg',
+    show: false,
+  },
+
+  onLoad () {
+    const { userinfo } = app.globalData
+    this.setData({
+      nickname: userinfo. nickname,
+      avatar: userinfo.avatar,
+    })
+  },
 
   onShow () {
     this.getTabBar().setData({
       active: 1,
     })
-  }
+  },
+
+  showLoginDialog () {
+    const { nickname, avatar } = this.data
+    if (nickname === '点击登录' || avatar === '/icons/user.svg') {
+      this.setData({
+        show: true,
+      })
+    }
+  },
+
+  confirmLogin (e) {
+    const { nickname, avatar } = e.detail
+    if (nickname === '') {
+      Toast.fail('昵称不能为空')
+      return
+    }
+    if (avatar === '') {
+      Toast.fail('头像不能为空')
+      return
+    }
+    this.setData({
+      nickname,
+      avatar,
+    })
+    wx.setStorageSync('userinfo', {
+      nickname,
+      avatar,
+    })
+  },
+
+  editPassword () {},
+
+  editBindPhone () {},
+
+  exitLogin () {
+    wx.removeStorageSync('token')
+    wx.removeStorageSync('userinfo')
+    app.globalData.token = ''
+    app.globalData.userinfo.nickname = '点击登录'
+    app.globalData.userinfo.avatar = '/icons/user.svg'
+    wx.reLaunch({
+      url: '/pages/register/register',
+    })
+  },
 
 })
