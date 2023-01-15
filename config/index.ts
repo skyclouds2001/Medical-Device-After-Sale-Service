485a1d2650a3ca252fbb9b4bd5fc151a3d17be7d
skyclouds2001 <skyclouds2001@163.com>
2023-01-15 12:55:27 +0800

    feat: update config

--- config/index.ts
+++ config/index.ts
@@ -7,14 +7,14 @@ export const APPLICATION_VERSION: string = '0.0.0'
 /** 网络请求HOST */
 export const SERVER_HOST: string = 'http://aftersale.divergentcloud.com'
 
-/** 默认请求无需鉴权接口白名单，若修改需同时修改后端 */
+/** 默认请求无需鉴权接口白名单，!!若修改需同时修改后端!! */
 export const WHITE_LIST: string[] = ['/wizz/aftersale/account/customer/login', '/wizz/aftersale/account/customer/authenticate']
 
-/** 默认单页的容量，若修改需同时修改后端 */
+/** 默认单页的容量，!!若修改需同时修改后端!! */
 export const DEFAULT_PAGE_SIZE: number = 10
 
-/** 默认登录Session过期时间，若修改需同时修改后端 */
+/** 默认登录Session过期时间，!!若修改需同时修改后端!! */
 export const SESSION_EXPIRE: number = 3 * 24 * 60 * 60 * 1000
 
-/** 调用微信客服接口企业ID */
+/** 微信客服企业ID */
 export const CUSTOMER_SERVICE_COMPANY_ID: string = 'ww562acecf4b6b8caf'
