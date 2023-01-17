687950694fe34d097aa26595be996fe70a16904c
skyclouds2001 <skyclouds2001@163.com>
2023-01-17 15:53:39 +0800

    feat: update history page

--- pages/history/history.ts
+++ pages/history/history.ts
@@ -1,7 +1,35 @@
-Page<{}, {}>({
+import Toast from '@vant/weapp/toast/toast'
+import { getUserWorkOrder } from '@/apis/work-order'
 
-  data: {},
+const app = getApp<App>()
 
-  onLoad() {},
+Page<{
+  wos: WorkOrder[]
+}, {
+  loadWorkOrderList: () => void
+}>({
+
+  data: {
+    wos: [],
+  },
+
+  onLoad() {
+    this.loadWorkOrderList()
+  },
+
+  async loadWorkOrderList () {
+    try {
+      const res = await getUserWorkOrder(app.globalData.id)
+      if (res.code === 0) {
+        this.setData({
+          wos: res.data,
+        })
+      } else {
+        Toast.fail(res.data.toString())
+      }
+    } catch (err) {
+      Toast.fail('加载历史工单列表失败')
+    }
+  },
 
 })
