66f5398789ecbdfeac87b7213fded19b3a4d12cf
skyclouds2001 <skyclouds2001@163.com>
2023-03-09 21:32:02 +0800

    feat: done file list page basically

--- pages/document-list/index.ts
+++ pages/document-list/index.ts
@@ -1,6 +1,28 @@
-Page<{}, {}>({
+interface File {
+  id: number
+  name: string
+  type: 'DOCX' | 'XLSX' | 'PPTX' | 'PDF' | 'unknown'
+  size: string
+  date: string
+}
 
-  data: {},
+const files: File[] = [
+  {
+    id: 1,
+    name: '产品详情文件1',
+    type: 'DOCX',
+    size: '12.9MB',
+    date: '2023-3-6',
+  },
+]
+
+Page<{
+  files: File[]
+}, {}>({
+
+  data: {
+    files: [...files],
+  },
 
   onLoad() {},
 
