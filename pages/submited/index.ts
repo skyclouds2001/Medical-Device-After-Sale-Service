1c58e02411e97812a2a9cbb78bf91fa78a781976
skyclouds2001 <skyclouds2001@163.com>
2023-03-09 22:07:26 +0800

    feat: update submit page

--- pages/submited/index.ts
+++ pages/submited/index.ts
@@ -1,9 +1,8 @@
-Page<{
-  /**
-   * 提交后按钮文字
-   */
-  text: '返回首页' | '转入人工客服'
-}, {
+import Toast from '@vant/weapp/toast/toast'
+import { getKfLink } from '@/apis/consult'
+import { CUSTOMER_SERVICE_COMPANY_ID } from '@/config/index'
+
+Page<{}, {
   /**
    * 工单对应服务ID
    */
@@ -14,30 +13,47 @@ Page<{
   pid: number
 
   /**
-   * 完成提交工单回调方法
+   * 打开客服回调方法
    */
-  doneSubmit: () => Promise<void>
+  openCustomerService: () => void
+  /**
+   * 返回首页回调方法
+   */
+  returnHomePage: () => void
 }>({
 
-  data: {
-    text: '返回首页',
-  },
+  data: {},
 
   onLoad (options: Record<'sid' | 'pid', string>) {
     this.sid = parseInt(options.sid)
     this.pid = parseInt(options.pid)
-
-    if (this.sid === 5) {
-      this.setData({
-        text: '转入人工客服',
-      })
-    }
   },
 
   sid: 0,
   pid: 0,
 
-  async doneSubmit () {
+  async openCustomerService () {
+    try {
+      const res = await getKfLink(this.pid, this.sid as 1 | 2 | 3)
+      if (res.code === 0) {
+        wx.openCustomerServiceChat({
+          extInfo: {
+            url: res.data.kf_link,
+          },
+          corpId: CUSTOMER_SERVICE_COMPANY_ID,
+          fail: (err) => {
+            Toast.fail(err.errMsg)
+          },
+        })
+      } else {
+        Toast.fail(res.data?.toString() ?? '获取客服链接失败')
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  },
+
+  returnHomePage () {
     wx.switchTab({
       url: '/pages/home/index',
     })
