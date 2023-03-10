cd1028b15fc9b7aaca46a79fe1a2b961a4645159
skyclouds2001 <skyclouds2001@163.com>
2023-03-10 11:13:32 +0800

    feat: update login page

--- pages/login/index.ts
+++ pages/login/index.ts
@@ -1,7 +1,5 @@
 import Toast from '@vant/weapp/toast/toast'
 import { login, auth } from '@/apis/admin'
-import { getCompanyList } from '@/apis/company'
-import { DEFAULT_PAGE_SIZE } from '@/config/index'
 import { setStorage } from '@/lib/storage'
 import { validatePhone, validatePassword } from '@/utils/validate'
 
@@ -9,26 +7,13 @@ const app = getApp<App>()
 
 Page<{
   /**
-   * 公司列表
-   */
-  companies: Company[]
-  /**
-   * 控制公司选择弹窗显示与否
-   */
-  show: boolean
-
-  /**
    * 手机号
    */
-  phone: string
+  pwd: string
   /**
    * 用户姓名
    */
   name: string
-  /**
-   * 用户所属公司
-   */
-  company: Company
 }, {
   /**
    * 用户openid
@@ -36,108 +21,56 @@ Page<{
   openid: string
 
   /**
-   * 加载公司列表方法
+   * 用户自动登录方法
    */
-  loadCompanyList: () => Promise<void>
-
+  autoLogin: () => Promise<void>
   /**
-   * 用户登录回调方法
+   * 用户手动登录方法
    */
   userLogin: () => Promise<void>
-  /**
-   * 用户注册回调方法
-   */
-  userRegister: () => Promise<void>
-
-  /**
-   * 选择企业回调方法
-   *
-   * @param e 点击事件
-   */
-  handleManageSelector: (e: WechatMiniprogram.TouchEvent<{ value: Company }, { flag: boolean }>) => void
 }>({
 
   data: {
-    companies: [],
-    show: false,
-
     name: '',
-    phone: '',
-    company: {
-      company_id: -1,
-      company_name: '',
-    },
+    pwd: '',
   },
 
   onLoad() {
-    this.loadCompanyList()
-    this.userLogin()
+    this.autoLogin()
   },
 
   openid: '',
 
-  async loadCompanyList() {
-    try {
-      let page = 1
-      const res = await getCompanyList(page, true)
-      if (res.code !== 0) throw res.data.toString()
-      ++page
-
-      const { total_num: num } = res.data
-      this.setData({
-        companies: [...res.data.company_list],
-      })
-
-      while (page <= num / DEFAULT_PAGE_SIZE + 1) {
-        const res = await getCompanyList(page, false)
-        if (res.code !== 0) throw res.data.toString()
-        this.setData({
-          companies: [...this.data.companies, ...res.data.company_list]
-        })
-        ++page
-      }
-    } catch {
-      Toast.fail('加载企业列表失败')
-    }
-  },
-
-  handleManageSelector(e) {
-    if (e.type === 'confirm') {
-      this.setData({
-        company: e.detail.value,
-        show: Boolean(e.mark?.flag),
-      })
-    } else {
-      this.setData({
-        show: Boolean(e.mark?.flag),
-      })
-    }
-  },
-
-  async userLogin() {
+  async autoLogin() {
     try {
       Toast.loading({
         message: '登录中',
         duration: 0,
         mask: true,
       })
+
       const { code } = await wx.login()
       const res = await login(code)
+
       if (res.code === 0) {
         const { token, customer_id, company_name, email: phone } = res.data
+
         setTimeout(() => {
           Toast.clear()
           Toast.success('登录成功')
-        }, 1000)
+        }, 500)
+
         setTimeout(() => {
           wx.switchTab({
             url: '/pages/home/index',
           })
-        }, 2000)
+        }, 2500)
+
         app.globalData.token = token
         app.globalData.id = customer_id
         app.globalData.company = company_name
         app.globalData.phone = phone
+
         setStorage(
           {
             key: 'token',
@@ -157,6 +90,7 @@ Page<{
           },
         )
       } else if (res.code === 1070) {
+        // todo
         Toast.clear()
         this.openid = res.data.toString()
       } else {
@@ -167,13 +101,9 @@ Page<{
     }
   },
 
-  async userRegister() {
-    const { phone, name, company } = this.data
+  async userLogin() {
+    const { pwd: phone, name } = this.data
 
-    if (company.company_id === -1 || company.company_name === '') {
-      Toast.fail('需选择所属企业')
-      return
-    }
     if (!validatePhone(phone)) {
       Toast.fail('电话格式不正确')
       return
@@ -184,20 +114,24 @@ Page<{
     }
 
     try {
-      const res = await auth(company?.company_id, name, phone, this.openid)
+      const res = await auth(34, name, phone, this.openid)
       if (res.code === 0) {
         Toast.success('注册成功')
+
         const { code } = await wx.login()
         const res = await login(code)
+
         if (res.code === 0) {
           const { token, customer_id, company_name, email: phone } = res.data
           wx.switchTab({
             url: '/pages/index/index',
           })
+
           app.globalData.token = token
           app.globalData.id = customer_id
           app.globalData.company = company_name
           app.globalData.phone = phone
+
           setStorage(
             {
               key: 'token',
