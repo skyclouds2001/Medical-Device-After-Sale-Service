fa6ece89de82528ec0c220b69f1016b5a08771fd
skyclouds2001 <skyclouds2001@163.com>
2023-03-10 13:04:30 +0800

    feat: add get file list support

--- pages/document-list/index.ts
+++ pages/document-list/index.ts
@@ -1,29 +1,59 @@
-interface File {
-  id: number
-  name: string
-  type: 'DOCX' | 'XLSX' | 'PPTX' | 'PDF' | 'unknown'
-  size: string
-  date: string
-}
-
-const files: File[] = [
-  {
-    id: 1,
-    name: '产品详情文件1',
-    type: 'DOCX',
-    size: '12.9MB',
-    date: '2023-3-6',
-  },
-]
+import Toast from '@vant/weapp/toast/toast'
+import { getFileList } from '@/apis/file'
 
 Page<{
   files: File[]
-}, {}>({
+}, {
+  /**
+   * 文件列表所属服务 ID
+   *
+   * 仅可能为5-培训或6-软件升级(null为初始值)
+   */
+  sid: null | 5 | 6
+  /**
+   * 加载文件列表
+   *
+   * @param type 文件的服务类型
+   */
+  loadFileList: (type: 0 | 1) => void
+}>({
 
   data: {
-    files: [...files],
+    files: [],
   },
 
-  onLoad() {},
+  onLoad (options: { sid: '5' | '6' }) {
+    const sid = parseInt(options.sid)
+
+    if (sid !== 5 && sid !== 6) {
+      Toast.fail('非法的服务类型')
+
+      setTimeout(() => {
+        wx.navigateBack()
+      }, 1500)
+
+      return
+    }
+
+    this.sid = sid
+    this.loadFileList(sid - 5 as 0 | 1)
+  },
+
+  sid: null,
+
+  async loadFileList (type: 0 | 1) {
+    try {
+      const res = await getFileList(type)
+      if (res.code === 0) {
+        this.setData({
+          files: res.data.file_info_list ?? []
+        })
+      } else {
+        Toast.fail(res.data?.toString() ?? '获取文件列表失败')
+      }
+    } catch {
+      Toast.fail('获取文件列表失败')
+    }
+  },
 
 })
