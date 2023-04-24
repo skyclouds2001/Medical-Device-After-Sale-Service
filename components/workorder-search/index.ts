352a4e87f6f1874ca0ef7de39d5beebfa623bc3a
skyclouds2001 <skyclouds2001@163.com>
2023-04-24 12:33:18 +0800

    feat: update work order history

--- components/workorder-search/index.ts
+++ components/workorder-search/index.ts
@@ -2,82 +2,60 @@ import { getAllProductModels } from '@/apis/product'
 import type Product from '@/models/Product'
 
 Component<{
-  /** 产品列表 */
-  products: Product[]
   /** 产品筛选 */
   product: number
   /** 排序方式 */
-  order: -1 | 0 | 1
+  order: 0 | 1
   /** 工单状态 */
   status: -1 | 0 | 1
+
+  /** 产品列表 */
+  products: Product[]
 }, {}, {
   /** 选取产品回调方法 */
   handleProductSelect: (e: WechatMiniprogram.TouchEvent<{}, { id: number }>) => void
   /** 选取时间排序回调方法 */
   handleTimeSort: (e: WechatMiniprogram.TouchEvent<{}, { id: 0 | 1 }>) => void
   /** 选取工单状态回调方法 */
-  handleWorkOrderStatusSelect: (e: WechatMiniprogram.TouchEvent<{}, { id: 0 | 1 }>) => void
-  /** 清空筛选排序回调方法 */
-  handleReset: () => void
-  /** 启用筛选排序回调方法 */
-  handleFilter: () => void
+  handleWorkOrderStatusSelect: (e: WechatMiniprogram.TouchEvent<{}, { id: -1 | 0 | 1 }>) => void
 }>({
 
   properties: {},
 
   data: {
-    products: [],
     product: -1,
-    order: -1,
+    order: 0,
     status: -1,
+
+    products: [],
   },
 
   methods: {
     handleProductSelect (e) {
       const { id } = e.mark!
-      const { product } = this.data
+      const { status, order } = this.data
+      this.triggerEvent('filter', { status, order, product: id })
       this.setData({
-        product: id === product ? -1 : id,
+        product: id,
       })
     },
 
     handleTimeSort (e) {
       const { id } = e.mark!
-      const { order } = this.data
+      const { status, product } = this.data
+      this.triggerEvent('filter', { status, order: id, product })
       this.setData({
-        order: id === order ? -1 : id,
+        order: id,
       })
     },
 
     handleWorkOrderStatusSelect (e) {
       const { id } = e.mark!
-      const { status } = this.data
+      const { order, product } = this.data
+      this.triggerEvent('filter', { status: id, order, product })
       this.setData({
-        status: id === status ? -1 : id,
+        status: id,
       })
-    },
-
-    handleReset () {
-      this.setData({
-        order: -1,
-        status: -1,
-        show: false,
-      })
-      this.triggerEvent('reset')
-    },
-
-    handleFilter () {
-      const { order, status, product } = this.data
-      let data = {} as { order?: 0 | 1, status?: 0 | 1, product?: number }
-      if (order !== -1) data.order = order
-      if (status !== -1) data.status = status
-      if (product !== -1) data.product = product
-
-      this.setData({
-        show: false,
-      })
-
-      this.triggerEvent('filter', data)
     },
   },
 
