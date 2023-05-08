a5533a22ea4f72160048f639072f991cdcc22407
skyclouds2001 <skyclouds2001@163.com>
2023-05-08 11:03:49 +0800

    feat: update validate phone

--- utils/validate.ts
+++ utils/validate.ts
@@ -5,5 +5,5 @@
  * @returns 校验手机号结果
  */
 export const validatePhone = (phone: string) => {
-  return /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(phone) || /^0?\d{2,3}-?\d{7,8}$/.test(phone)
+  return /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1}))+\d{8})$/.test(phone) || /^0?\d{2,3}-?\d{7,8}$/.test(phone) || /^\d{7,8}$/.test(phone)
 }
